668711a6f5a075379bef79e02adb2cdbacfcefd7
ilarian <ilari.aukusti@gmail.com>
2025-09-10 11:07:13 +0300

    uibuild

--- osa3/puhelinluettelobackend/index.js
+++ osa3/puhelinluettelobackend/index.js
@@ -9,6 +9,7 @@ app.use(express.json())
 morgan.token('body', (req, res) => {
     if(req.body) return JSON.stringify(req.body)
 })
+app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
