22aacde99c60a11793002b73d4251bd98ebcb949
ilarian <ilari.aukusti@gmail.com>
2025-09-16 11:11:54 +0300

    Part 4.1-4.2

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const {DB_URI} = require('./utils/config')
+const mongoose = require("mongoose")
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+mongoose.connect(DB_URI)
+
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
