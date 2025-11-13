be7e6df587effa83f5de05a289ec3547e04cdf89
ilarian <ilari.aukusti@gmail.com>
2025-11-13 12:33:18 +0200

    part 8.8-8.12

--- osa8/library/index.js
+++ osa8/library/index.js
@@ -126,7 +126,7 @@ const typeDefs = `
     title: String!
     published: Int!
     author: String!
-    genres: [String!]!): Book!
+    genres: [String!]!): Book
 
     editAuthor(name: String!, setBornTo: Int): Author
   }
